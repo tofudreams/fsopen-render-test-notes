7c595ce251da2352c8e148d895d2fbb3af28a0d9
tofudreams <jams676@gmail.com>
2024-08-25 00:57:49 +0200

    token based auth

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ const app = express()
 const cors = require('cors')
 const usersRouter = require('./controllers/users')
 const notesRouter = require('./controllers/notes')
+const loginRouter = require('./controllers/login')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
+
 mongoose.set('strictQuery', false)
 
 logger.info('connecting to', config.MONGODB_URI)
@@ -26,6 +28,7 @@ app.use(express.json())
 app.use(middleware.requestLogger)
 
 app.use('/api/notes', notesRouter)
+app.use('/api/login', loginRouter)
 app.use('/api/users', usersRouter)
 
 app.use(middleware.unknownEndpoint)
